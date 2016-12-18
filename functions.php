534989c7980cac5da5482e2f3f4e0c0df6a33755
Anthony Skelton <anthony@Anthonys-MacBook-Pro.local>
2016-12-17 18:49:54 -0800

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description
+ *
+ * @package   ${NAMESPACE}
+ * @since     1.0.0
+ * @author    ajskelton
+ * @link      anthonyskelton.com
+ * @license   GNU General Public License 2.0+
+ */
